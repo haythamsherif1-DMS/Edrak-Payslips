84d5dafea2e0433c314c40be643f40ab256396f9
haythamsherif1-DMS <haythamsherif1@gmail.com>
2026-02-25 10:27:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,22 @@
-self.addEventListener('install', (e) => {
+const cacheName = 'edrak-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('edrak-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json'
-    ])),
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
